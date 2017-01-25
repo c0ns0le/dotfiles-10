be13d63d2311460a1202455b509b489150c4edc3
Ali Tayarani <ali.tayarani@tapjoy.com>
2017-01-25 15:51:15 -0500

    add more rules

--- thefuck/settings.py
+++ thefuck/settings.py
@@ -1,5 +1,5 @@
-rules = ['git_push', 'no_command']
+rules = ['aws_cli', 'git_checkout', 'git_push', 'no_command']
 require_confirmation = True
 wait_command = 10
 no_colors = False
-priority = {'git_push': 100, 'no_command': 9999}
+priority = {'aws_cli': 100, 'git_checkout': 100, 'git_push': 100, 'no_command': 9999}
